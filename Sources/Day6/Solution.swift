3550739117b93d2a815b7d123f8bfe133997c22e
Joshua Harding <joshua.harding@four.io>
2023-12-10 14:46:09 +1030

    Split up all previous solutions so input files are separate from solution code.

--- Sources/Day6/Solution.swift
+++ Sources/Day6/Solution.swift
@@ -1,51 +1,40 @@
 import Foundation
 
-print("Day 6.1: \(Day6.part1)")
-print("Day 6.2: \(Day6.part2)")
+print("Part 1: \(part1(Input.lines))")
+print("Part 2: \(part2(Input.lines))")
 
-struct Day6 {
-    static var part1: Int {
-        let lines = input.split(separator: "\n")
-            .map { $0.split(separator: " ").dropFirst().map { Double($0)! } }
-        
-        let races = (0..<lines[0].count).map { (time: lines[0][$0], distance: lines[1][$0]) }
-        
-        return races.map { race in
-            if let (min, max) = solveQuadratic(a: 1, b: -race.time, c: race.distance) {
-                return Int(ceil(max) - ceil(min))
-            }
-            
-            return 0
-        }.reduce(1, *)
-    }
+func part1(_ input: [String]) -> Int {
+    let lines = input
+        .map { $0.split(separator: " ")
+        .dropFirst()
+        .map { Double($0)! } }
     
-    static var part2: Int {
-        let race = input.replacingOccurrences(of: " ", with: "")
-            .split(separator: "\n")
-            .map { Double($0.split(separator: ":")[1])! }
-        
-        // (tMax - tHeld) * tHeld = distance
-        // tMax * tHeld - tHeld**2 = distance
-        // tx - x^2 = d
-        // 0 = x^2 - tx + d   where t == time, d == distance
-        
-        if let (min, max) = solveQuadratic(a: 1, b: -race[0], c: race[1]) {
+    let races = (0..<lines[0].count).map { (time: lines[0][$0], distance: lines[1][$0]) }
+    
+    return races.map { race in
+        if let (min, max) = solveQuadratic(a: 1, b: -race.time, c: race.distance) {
             return Int(ceil(max) - ceil(min))
         }
         
         return 0
+    }.reduce(1, *)
+}
+
+func part2(_ input: [String]) -> Int {
+    let race = input
+        .map { $0.replacingOccurrences(of: " ", with: "") }
+        .map { Double($0.split(separator: ":")[1])! }
+    
+    // (tMax - tHeld) * tHeld = distance
+    // tMax * tHeld - tHeld**2 = distance
+    // tx - x^2 = d
+    // 0 = x^2 - tx + d   where t == time, d == distance
+    
+    if let (min, max) = solveQuadratic(a: 1, b: -race[0], c: race[1]) {
+        return Int(ceil(max) - ceil(min))
     }
     
-    
-    static let example = """
-        Time:      7  15   30
-        Distance:  9  40  200
-        """
-    
-    static let input = """
-        Time:        48     93     84     66
-        Distance:   261   1192   1019   1063
-        """
+    return 0
 }
 
 func solveQuadratic(a: Double, b: Double, c: Double) -> (Double, Double)? {
@@ -60,4 +49,14 @@ func solveQuadratic(a: Double, b: Double, c: Double) -> (Double, Double)? {
     let minus = (-b - squareRootTerm) / (2 * a)
     
     return plus < minus ? (plus, minus) : (minus, plus)
+}
+
+struct Input {
+    static let lines: [String] = {
+        var lines: [String] = []
+        while let line = readLine() {
+            lines.append(line)
+        }
+        return lines
+    }()
 }
