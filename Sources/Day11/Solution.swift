a213ea2f5705b327a357322d01f002c42f6f798b
Joshua Harding <joshua.harding@four.io>
2023-12-12 10:13:32 +1030

    Day 11 cleanup

--- Sources/Day11/Solution.swift
+++ Sources/Day11/Solution.swift
@@ -1,45 +1,51 @@
 import Foundation
 
-var populatedRows: Set<Int> = Set()
-var populatedColumns: Set<Int> = Set()
+let (total, gaps) = calculateDistances(Input.lines)
 
-let input = Input.lines.enumerated()
-    .flatMap {
-        let row = $0.offset
-        return $0.element
-            .split(separator: "")
-            .enumerated()
-            .compactMap {
-                if ($0.element == "#") {
-                    let column = $0.offset
-                    populatedRows.insert(row)
-                    populatedColumns.insert(column)
-                    
-                    return (row: row, column: column)
+print("Part 1: \(total + gaps)")
+print("Part 2: \(total + gaps * (1000000 - 1))")
+
+func calculateDistances(_ lines: [String]) -> (total: Int, gaps: Int) {
+    var populatedRows: Set<Int> = Set()
+    var populatedColumns: Set<Int> = Set()
+
+    let input = lines.enumerated()
+        .flatMap {
+            let row = $0.offset
+            return $0.element
+                .split(separator: "")
+                .enumerated()
+                .compactMap {
+                    if ($0.element == "#") {
+                        let column = $0.offset
+                        populatedRows.insert(row)
+                        populatedColumns.insert(column)
+                        
+                        return (row: row, column: column)
+                    }
+
+                    return nil
                 }
+        }
 
-                return nil
-            }
+    var gaps = 0
+
+    var total = 0
+    for i in 0..<input.count - 1 {
+        let (startRow, startColumn) = input[i]
+        for j in (i+1)..<input.count {
+            let (endRow, endColumn) = input[j]
+            
+            total += abs(endRow - startRow)
+            total += abs(endColumn - startColumn)
+            
+            gaps += stride(from: startRow, to: endRow, by: startRow < endRow ? 1 : -1).filter { !populatedRows.contains($0) }.count
+            gaps += stride(from: startColumn, to: endColumn, by: startColumn < endColumn ? 1 : -1).filter { !populatedColumns.contains($0) }.count
+        }
     }
 
-var spaces = 0
-
-var total = 0
-for i in 0..<input.count - 1 {
-    for j in (i+1)..<input.count {
-        let (startRow, endRow) = input[i].row < input[j].row ? (input[i].row, input[j].row) : (input[j].row, input[i].row)
-        let (startColumn, endColumn) = input[i].column < input[j].column ? (input[i].column, input[j].column) : (input[j].column, input[i].column)
-        
-        total += endRow - startRow
-        total += endColumn - startColumn
-        
-        spaces += (startRow..<endRow).filter { !populatedRows.contains($0) }.count
-        spaces += (startColumn..<endColumn).filter { !populatedColumns.contains($0) }.count
-    }
+    return (total, gaps)
 }
-
-print("Part 1: \(total + spaces)")
-print("Part 2: \(total + spaces * (1000000 - 1))")
 
 struct Input {
     static let lines: [String] = {
